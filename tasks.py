8683d022e360be24eeadbcdc3979018db9f3cce0
tonnitommi <tommi.holmgren@gmail.com>
2023-11-27 09:11:36 +0200

    fixed deps

--- tasks.py
+++ tasks.py
@@ -1,25 +1,35 @@
 from robocorp.tasks import task
 from robocorp import vault
-import os
+
 from llama_index import SimpleDirectoryReader, StorageContext
 from llama_index.indices.vector_store import VectorStoreIndex
 from llama_index.vector_stores import TimescaleVectorStore
+
 import textwrap
+import os
 
 @task
 def my_timescale_loader():
+    """Load all data from work item files as vector embeddings to Timescale Vector.
+    Uses Llamaindex Simple Directory Reader, supporting multiple file types."""
+
+    # Set up all the credentials from Robocorp Vault
     openai_credentials = vault.get_secret("OpenAI")
     os.environ["OPENAI_API_KEY"] = openai_credentials["key"]
     timescale_credentials = vault.get_secret("Timescale")
     TIMESCALE_SERVICE_URL = timescale_credentials["service-url"]
 
+    # Get work item files and store to local data folder temporatily.
+    # TODO
+
+    # Read documents
     documents = SimpleDirectoryReader("data").load_data()
     print("Document ID:", documents[0].doc_id)
 
     # Create a TimescaleVectorStore to store the documents
     vector_store = TimescaleVectorStore.from_params(
         service_url=TIMESCALE_SERVICE_URL,
-        table_name="state_of_the_union",
+        table_name="timescale_robocorp_example",
     )
 
     # Create a new VectorStoreIndex using the TimescaleVectorStore
